930eb0fc06981dca7c6a50a2d4c81da2f95a840a
Taha Manshoor Dar <tahadar221177@gmail.com>
2026-05-23 16:54:26 +0500

    Added the models and connected with Database

---
+++ BlogApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using BlogApp.Models;
+
+namespace BlogApp.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<IdentityUser>(options)
+    {
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
---
+++ BlogApp/Models/Comment.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlogApp.Models
+{
+    public class Comment
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "The UserName is Required")]
+        [MaxLength(100, ErrorMessage = "The UserName cannot exceed 100 characters")]
+        public string UserName { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime CommentDate { get; set; }
+        [Required(ErrorMessage = "The Content is Required")]
+        public string Content { get; set; }
+        [ForeignKey("Post")]
+        public int PostId { get; set; }
+        public Post Post { get; set; }
+    }
+}
